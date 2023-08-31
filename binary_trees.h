633fc025d62fe08c7fa2c22977a4a9e14f321694
Abubacer <azizdesignislife@gmail.com>
2023-08-31 01:09:03 +0000

    Binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -63,6 +63,7 @@ bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
+bst_t *bst_minval_node(bst_t *node);
 int binary_tree_is_avl(const binary_tree_t *tree);
 int is_valid_avl(const binary_tree_t *node, const int *min, const int *max);
 avl_t *avl_insert(avl_t **tree, int value);
